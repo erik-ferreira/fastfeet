6d2bfece3fda7276941489ee8f22622a3216b246
erik-ferreira <erikdesouzagf@gmail.com>
2026-07-14 23:10:42 -0300

    create use case to register user

--- src/domain/delivery-and-order/application/use-cases/register-user.ts
+++ src/domain/delivery-and-order/application/use-cases/register-user.ts
@@ -1,7 +1,47 @@
 import { Injectable } from "@nestjs/common"
-import { UserRepository } from "../repositories/user-repository"
+
+import { Either, left, right } from "@/core/either"
+
+import { User } from "@/domain/delivery-and-order/enterprise/entities/user"
+import { HashGenerator } from "@/domain/delivery-and-order/application/cryptography/hash-generator"
+import { UserRepository } from "@/domain/delivery-and-order/application/repositories/user-repository"
+
+interface RegisterUserUseCaseRequest {
+  name: string
+  cpf: string
+  password: string
+}
+
+type RegisterUserUseCaseResponse = Either<string, { user: User }>
 
 @Injectable()
 export class RegisterUserUseCase {
-  constructor(private userRepository: UserRepository) {}
+  constructor(
+    private userRepository: UserRepository,
+    private hashGenerator: HashGenerator,
+  ) {}
+
+  async execute({
+    name,
+    cpf,
+    password,
+  }: RegisterUserUseCaseRequest): Promise<RegisterUserUseCaseResponse> {
+    const userWithSameCpf = await this.userRepository.findByCpf(cpf)
+
+    if (userWithSameCpf) {
+      return left("dale")
+    }
+
+    const hashedPassword = await this.hashGenerator.hash(password)
+
+    const user = User.create({
+      name,
+      cpf,
+      password: hashedPassword,
+    })
+
+    await this.userRepository.create(user)
+
+    return right({ user })
+  }
 }
