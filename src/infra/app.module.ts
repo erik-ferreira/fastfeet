2c89e2e8b7e6af079eafe5ee5923bd7feb3190e0
erik-ferreira <erikdesouzagf@gmail.com>
2026-07-14 22:48:29 -0300

    feat: implement auth module

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -4,12 +4,14 @@ import { ConfigModule } from "@nestjs/config"
 import { envSchema } from "@/infra/env/env"
 import { EnvModule } from "./env/env.module"
 import { HttpModule } from "./http/http.module"
+import { AuthModule } from "./auth/auth.module"
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
     }),
+    AuthModule,
     EnvModule,
     HttpModule,
   ],
