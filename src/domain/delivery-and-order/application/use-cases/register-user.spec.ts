80479d7a3d5230e72bbc2bd68cc05a529d87de2a
erik-ferreira <erikdesouzagf@gmail.com>
2026-07-16 23:13:12 -0300

    test use case to register user

---
+++ src/domain/delivery-and-order/application/use-cases/register-user.spec.ts
@@ -0,0 +1,45 @@
+import { FakeHasher } from "@/test/cryptography/fake-hasher"
+
+import { InMemoryUserRepository } from "@/test/repositories/in-memory-user-repository"
+
+import { RegisterUserUseCase } from "./register-user"
+
+let fakeHasher: FakeHasher
+let inMemoryUserRepository: InMemoryUserRepository
+
+let sut: RegisterUserUseCase
+
+describe("Register user", () => {
+  beforeEach(() => {
+    inMemoryUserRepository = new InMemoryUserRepository()
+    fakeHasher = new FakeHasher()
+
+    sut = new RegisterUserUseCase(inMemoryUserRepository, fakeHasher)
+  })
+
+  it("should be able to register a new student", async () => {
+    const result = await sut.execute({
+      name: "John Doe",
+      cpf: "00000000000",
+      password: "12345",
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(result.value).toEqual({
+      user: inMemoryUserRepository.items[0],
+    })
+  })
+
+  it("should hash student password upon registration", async () => {
+    const result = await sut.execute({
+      name: "John Doe",
+      cpf: "00000000000",
+      password: "123456",
+    })
+
+    const hashedPassword = await fakeHasher.hash("123456")
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryUserRepository.items[0].password).toEqual(hashedPassword)
+  })
+})
---
+++ test/repositories/in-memory-user-repository.ts
@@ -0,0 +1,21 @@
+import { User } from "@/domain/delivery-and-order/enterprise/entities/user"
+
+import { UserRepository } from "@/domain/delivery-and-order/application/repositories/user-repository"
+
+export class InMemoryUserRepository extends UserRepository {
+  public items: User[] = []
+
+  async findByCpf(cpf: string) {
+    const user = this.items.find((user) => user.cpf === cpf)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create(user: User) {
+    this.items.push(user)
+  }
+}
