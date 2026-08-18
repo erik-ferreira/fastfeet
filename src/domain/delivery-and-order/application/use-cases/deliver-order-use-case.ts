0e7b7cb4cbbbd0bbde20dc4f6b6894412ac9e5e8
erik-ferreira <erikdesouzagf@gmail.com>
2026-08-18 00:26:34 -0300

    correction flow in use case to deliver order

--- src/domain/delivery-and-order/application/use-cases/deliver-order-use-case.ts
+++ src/domain/delivery-and-order/application/use-cases/deliver-order-use-case.ts
@@ -4,17 +4,21 @@ import { Either, left, right } from "@/core/either"
 
 import { Order } from "@/domain/delivery-and-order/enterprise/entities/order"
 import { OrderRepository } from "@/domain/delivery-and-order/application/repositories/order-repository"
+import { DeliveryDriversRepository } from "@/domain/delivery-and-order/application/repositories/delivery-drivers-repository"
+import { OrderAttachmentRepository } from "@/domain/delivery-and-order/application/repositories/order-attachment-repository"
 
 import { ResourceNotFoundError } from "@/core/errors/resource-not-found-error"
 import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+import { NotAllowedError } from "@/core/errors/not-allowed-error"
 
 interface DeliverOrderUseCaseRequest {
   orderId: string
   deliveryDriverId: string
+  attachmentId: string
 }
 
 type DeliverOrderUseCaseResponse = Either<
-  ResourceNotFoundError,
+  ResourceNotFoundError | NotAllowedError,
   {
     order: Order
   }
@@ -22,19 +26,46 @@ type DeliverOrderUseCaseResponse = Either<
 
 @Injectable()
 export class DeliverOrderUseCase {
-  constructor(private ordersRepository: OrderRepository) {}
+  constructor(
+    private ordersRepository: OrderRepository,
+    private deliveryDriversRepository: DeliveryDriversRepository,
+    private orderAttachmentRepository: OrderAttachmentRepository,
+  ) {}
 
   async execute({
     orderId,
     deliveryDriverId,
+    attachmentId,
   }: DeliverOrderUseCaseRequest): Promise<DeliverOrderUseCaseResponse> {
+    const deliveryDriver =
+      await this.deliveryDriversRepository.findById(deliveryDriverId)
+
+    if (!deliveryDriver) {
+      return left(new ResourceNotFoundError())
+    }
+
     const order = await this.ordersRepository.findById(orderId)
 
     if (!order) {
       return left(new ResourceNotFoundError())
     }
 
-    order.deliver(new UniqueEntityID(deliveryDriverId))
+    if (
+      order.status !== "WITHDRAWN" ||
+      !order.deliveryDriverId ||
+      !order.deliveryDriverId.equals(new UniqueEntityID(deliveryDriverId))
+    ) {
+      return left(new NotAllowedError())
+    }
+
+    const attachment =
+      await this.orderAttachmentRepository.findManyByOrderId(orderId)
+
+    if (!attachment) {
+      return left(new ResourceNotFoundError())
+    }
+
+    order.deliver(new UniqueEntityID(attachmentId))
 
     await this.ordersRepository.save(order)
 
