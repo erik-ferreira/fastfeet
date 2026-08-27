d9fc7436fa350fc6d45f9f0633be98889ef1cb5f
erik-ferreira <erikdesouzagf@gmail.com>
2026-08-26 23:52:48 -0300

    fix: apply corrections to run application

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -11,6 +11,7 @@ import { EventsModule } from "@/infra/events/events.module"
   imports: [
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
+      isGlobal: true,
     }),
     AuthModule,
     EnvModule,
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,8 +5,11 @@ import { CryptographyModule } from "@/infra/cryptography/cryptography.module"
 
 import { AuthenticateController } from "@/infra/http/controllers/auth/authenticate.controller"
 
+import { AuthenticateUserUseCase } from "@/domain/delivery-and-order/application/use-cases/authenticate-user"
+
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [AuthenticateController],
+  providers: [AuthenticateUserUseCase],
 })
 export class HttpModule {}
